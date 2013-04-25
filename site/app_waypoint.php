dd9fc3d5a404bbdff20d38ac5f7f5abfe6ce9d3d
unknown <dafoelle@3DLABOR12.in.fhkn.de>
2013-04-25 11:35:48 +0200

    waypoint modifications added.

--- site/app_waypoint.php
+++ site/app_waypoint.php
@@ -106,6 +106,116 @@
 			    </div>
 			</div><!-- Content -->
 			
+			<div id="appWrapper" align="center">
+			    <br />
+			    <h2>Weather information</h2>
+			    <br />
+			    <div class="container-fluid" align="center">
+		            	<div class="row well" style="margin-left: 15%;" align="center">
+		            		<div width: 600px;>
+								<div class="control-group">
+									<label class="control-label" >Wind strength</label>
+									<img src="../img/custom/windspeed0.png" />   
+									<input type="radio" name="windstrength" value="0" />
+									<img src="../img/custom/windspeed1.png" /> 
+									<input type="radio" name="windstrength" value="1" /> 	
+									<img src="../img/custom/windspeed2.png" /> 
+									<input type="radio" name="windstrength" value="2" />  
+									<img src="../img/custom/windspeed3.png" /> 
+									<input type="radio" name="windstrength" value="3" >
+									<img src="../img/custom/windspeed4.png" > 
+									<input type="radio" name="windstrength" value="4" >
+									<img src="../img/custom/windspeed5.png" > 
+									<input type="radio" name="windstrength" value="5" > 
+									<img src="../img/custom/windspeed14.png" > 
+									<input type="radio" name="windstrength" value="14" >
+									<img src="../img/custom/windspeed15.png" > 
+									<input type="radio" name="windstrength" value="15" >	
+			            		</div>
+								<div class="control-group">
+			            			<label class="control-label" >Clouds</label>
+									<img src="../img/custom/sonne.jpg" > 
+									<input type="radio" name="clouds" value="sonnig" selected="selected" >
+									<img src="../img/custom/grauewolke.jpg" > 
+									<input type="radio" name="clouds" value="bewölkt" >
+									<img src="../img/custom/wolkesonneblitz.jpg" > 
+									<input type="radio" name="clouds" value="wechselhaft" >
+									<img src="../img/custom/wolkesonne.jpg" > 
+									<input type="radio" name="clouds" value="sonnewolke" >
+									<img src="../img/custom/blitz.jpg" > 
+									<input type="radio" name="clouds" value="gewittrig" >
+			                    </div>
+							</div>
+							<div class="span4">
+			            		<div class="control-group">
+			            			<label class="control-label" >Wind direction</label>
+			            			<select name="winddirection" id="winddirection" style="width: 165px";>
+										<option value="nord" selected="selected">Nord</option>
+										<option value="nordost">Nord-Ost</option>
+										<option value="ost">Ost</option>
+										<option value="sued">Süd-Ost</option>
+										<option value="sued">Süd</option>
+										<option value="suedwest">Süd-West</option>
+										<option value="west">West</option>
+										<option value="nordwest">Nord-West</option>
+									</select>
+			                    </div>
+			                    <div class="control-group">
+			            			<label class="control-label" >Rain</label>
+			            			<select name="rain" id="rain" style="width: 165px";>
+										<option value="leicht">Leicht</option>
+										<option value="mittel">Mittel</option>
+										<option value="stark">Stark</option>
+										<option value="sehrstark">Sehr stark</option>
+									</select>
+			                    </div>
+		            		</div>
+							<div class="span4">
+								<div class="control-group">
+			            			<label class="control-label" >Air pressure</label>
+			            			<input class="input-medium" type="text" id="airpressure"/>
+			                    </div>
+		            		
+		            			<div class="control-group">
+			            			<label class="control-label" >Temperature</label> 
+									<input type="radio" name="massstab" value="celsius" >Grad Celsius
+									<input type="radio" name="massstab" value="fahrenheit" >Grad Fahrenheit
+			            			<input  type="text" id="temp" class="input-medium" />
+			            		</div>
+							</div>
+							<div class="span4">
+		            			<div class="control-group">
+			            			<label class="control-label" >Wave hight</label>
+			            			<input class="input-medium" type="text" id="wavehigh" />
+			                    </div>
+			                    <div class="control-group">
+			            			<label class="control-label" >Wave direction</label>
+			            			<select name="wavedirection" style="width: 165px";>
+										<option value="nord" selected="selected">Nord</option>
+										<option value="nordost">Nord-Ost</option>
+										<option value="ost">Ost</option>
+										<option value="sued">Süd-Ost</option>
+										<option value="sued">Süd</option>
+										<option value="suedwest">Süd-West</option>
+										<option value="west">West</option>
+										<option value="nordwest">Nord-West</option>
+									</select>
+			                    </div>
+							</div>
+							<div class="span4">
+								<div class="control-group">
+									<label class="control-label" >Uhrzeit</label>
+									<input class="input-medium"  type="time" />
+								</div>
+								<div class="control-group">
+									<label class="control-label" >Datum</label>
+									<input class="input-medium"  type="date" id="datum"/>
+								</div>
+							</div>
+		            	</div>    
+	            </div>
+			</form>
+			
 		</div><!-- Container -->
 		
 		<!-- Java Script -->
