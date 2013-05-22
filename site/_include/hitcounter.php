2d15e5523ee501456924d3fb88b635efae66d3b9
StefanW <stefan.waldraff@gmail.com>
2013-05-22 15:00:54 +0200

    Fixed hitcounter bug
    hitcounter now uses the correct file in include and not site folder

--- site/_include/hitcounter.php
+++ site/_include/hitcounter.php
@@ -1,11 +1,11 @@
 <?php
-	$hit_count = @file_get_contents('count.txt');
+	$hit_count = @file_get_contents('_include/count.txt');
 	echo "<div > 
 			<h4>Hitcounter</h4> 
 			<p>" .$hit_count."</p> 
 		</div>";
 	$hit_count++;
-	@file_put_contents('count.txt', $hit_count);
+	@file_put_contents('_include/count.txt', $hit_count);
 ?>
 
 <!--class='span2' style='background-color:deepskyblue;-->
