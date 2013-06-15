377e231b0cb678c1e711574f8eaf6357fdeb325e
StefanW <stefan.waldraff@gmail.com>
2013-06-15 02:38:00 +0200

    implemented php to set all weatherdata to NULL

---
+++ site/app_waypoint_default.php
@@ -0,0 +1,41 @@
+<?php
+
+	include('_include/connection.php');
+	
+	if (!$db_selected) {
+	    $err = array( "wnr" => 'Error: ' . mysql_error() );
+	    echo json_encode($err);
+	    exit;
+	}
+
+	$sql = "UPDATE seapal.wegpunkte SET wcc=NULL WHERE wnr=" . $_POST['wnr'] . ";";
+	@file_put_contents('_include/log.txt', $sql);
+	$result = mysql_query($sql, $conn);
+	$sql = "UPDATE seapal.wegpunkte SET icon=NULL WHERE wnr=" . $_POST['wnr'] . ";";
+	$result = mysql_query($sql, $conn);
+	$sql = "UPDATE seapal.wegpunkte SET temp=NULL  WHERE wnr=" . $_POST['wnr'] . ";";
+	$result = mysql_query($sql, $conn);
+	$sql = "UPDATE seapal.wegpunkte SET airpressure=NULL WHERE wnr=" . $_POST['wnr'] . ";";
+	$result = mysql_query($sql, $conn);
+	$sql = "UPDATE seapal.wegpunkte SET windspeed=NULL WHERE wnr=" . $_POST['wnr'] . ";";
+	$result = mysql_query($sql, $conn);
+	$sql = "UPDATE seapal.wegpunkte SET winddirection=NULL WHERE wnr=" . $_POST['wnr'] . ";";
+	$result = mysql_query($sql, $conn);
+	$sql = "UPDATE seapal.wegpunkte SET rain=NULL WHERE wnr=" . $_POST['wnr'] . ";";
+	$result = mysql_query($sql, $conn);
+	$sql = "UPDATE seapal.wegpunkte SET clouds=NULL WHERE wnr=" . $_POST['wnr'] . ";";
+	$result = mysql_query($sql, $conn);
+	$sql = "UPDATE seapal.wegpunkte SET wavehight=NULL WHERE wnr=" . $_POST['wnr'] . ";";
+	$result = mysql_query($sql, $conn);
+	$sql = "UPDATE seapal.wegpunkte SET wavedirection=NULL WHERE wnr=" . $_POST['wnr'] . ";";
+	$result = mysql_query($sql, $conn);
+	
+	if (!$result) {
+	    $err = array( "wnr" => 'Error: ' . mysql_error() );
+	    echo json_encode($err);
+	    exit;
+	}
+	
+	mysql_close($conn);
+
+?>
